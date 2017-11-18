e341b1e30ebc2bee1b3c2e9f7c569720cc05dfdb
Byron Oyarzún <xoopset@gmail.com>
2017-11-18 17:45:23 -0300

    template

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,16 +5,16 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>ERP UNAB</title>
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <link rel="stylesheet" href="http://expresstv.cl/theme/bower_components/bootstrap/dist/css/bootstrap.min.css">
-  <link rel="stylesheet" href="http://expresstv.cl/theme/bower_components/font-awesome/css/font-awesome.min.css">
-  <link rel="stylesheet" href="http://expresstv.cl/theme/bower_components/Ionicons/css/ionicons.min.css">
-  <link rel="stylesheet" href="http://expresstv.cl/theme/dist/css/AdminLTE.min.css">
-  <link rel="stylesheet" href="http://expresstv.cl/theme/dist/css/skins/_all-skins.min.css">
-  <link rel="stylesheet" href="http://expresstv.cl/theme/bower_components/morris.js/morris.css">
-  <link rel="stylesheet" href="http://expresstv.cl/theme/bower_components/jvectormap/jquery-jvectormap.css">
-  <link rel="stylesheet" href="http://expresstv.cl/theme/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
-  <link rel="stylesheet" href="http://expresstv.cl/theme/bower_components/bootstrap-daterangepicker/daterangepicker.css">
-  <link rel="stylesheet" href="http://expresstv.cl/theme/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+  <link rel="stylesheet" href="{{ request()->root() }}/theme/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="{{ request()->root() }}/theme/bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="{{ request()->root() }}/theme/bower_components/Ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="{{ request()->root() }}/theme/dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="{{ request()->root() }}/theme/dist/css/skins/_all-skins.min.css">
+  <link rel="stylesheet" href="{{ request()->root() }}/theme/bower_components/morris.js/morris.css">
+  <link rel="stylesheet" href="{{ request()->root() }}/theme/bower_components/jvectormap/jquery-jvectormap.css">
+  <link rel="stylesheet" href="{{ request()->root() }}/theme/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
+  <link rel="stylesheet" href="{{ request()->root() }}/theme/bower_components/bootstrap-daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="{{ request()->root() }}/theme/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
 <body class="hold-transition skin-blue fixed sidebar-mini">
@@ -41,13 +41,13 @@
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="http://expresstv.cl/theme/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+              <img src="{{ request()->root() }}/theme/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
               <span class="hidden-xs">Byron Oyarzún</span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
               <li class="user-header">
-                <img src="http://expresstv.cl/theme/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                <img src="{{ request()->root() }}/theme/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
 
                 <p>
                   Alexander Pierce - Web Developer
@@ -91,7 +91,7 @@
       <!-- Sidebar user panel -->
       <div class="user-panel">
         <div class="pull-left image">
-          <img src="http://expresstv.cl/theme/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+          <img src="{{ request()->root() }}/theme/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
           <p>Byron Oyarzún</p>
@@ -205,26 +205,26 @@
   </aside>
   <div class="control-sidebar-bg"></div>
 </div>
-<script src="http://expresstv.cl/theme/bower_components/jquery/dist/jquery.min.js"></script>
-<script src="http://expresstv.cl/theme/bower_components/jquery-ui/jquery-ui.min.js"></script>
+<script src="{{ request()->root() }}/theme/bower_components/jquery/dist/jquery.min.js"></script>
+<script src="{{ request()->root() }}/theme/bower_components/jquery-ui/jquery-ui.min.js"></script>
 <script>
   $.widget.bridge('uibutton', $.ui.button);
 </script>
-<script src="http://expresstv.cl/theme/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<script src="http://expresstv.cl/theme/bower_components/raphael/raphael.min.js"></script>
-<script src="http://expresstv.cl/theme/bower_components/morris.js/morris.min.js"></script>
-<script src="http://expresstv.cl/theme/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
-<script src="http://expresstv.cl/theme/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="http://expresstv.cl/theme/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-<script src="http://expresstv.cl/theme/bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
-<script src="http://expresstv.cl/theme/bower_components/moment/min/moment.min.js"></script>
-<script src="http://expresstv.cl/theme/bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
-<script src="http://expresstv.cl/theme/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
-<script src="http://expresstv.cl/theme/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-<script src="http://expresstv.cl/theme/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<script src="http://expresstv.cl/theme/bower_components/fastclick/lib/fastclick.js"></script>
-<script src="http://expresstv.cl/theme/dist/js/adminlte.min.js"></script>
-<script src="http://expresstv.cl/theme/dist/js/pages/dashboard.js"></script>
-<script src="http://expresstv.cl/theme/dist/js/demo.js"></script>
+<script src="{{ request()->root() }}/theme/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="{{ request()->root() }}/theme/bower_components/raphael/raphael.min.js"></script>
+<script src="{{ request()->root() }}/theme/bower_components/morris.js/morris.min.js"></script>
+<script src="{{ request()->root() }}/theme/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+<script src="{{ request()->root() }}/theme/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="{{ request()->root() }}/theme/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+<script src="{{ request()->root() }}/theme/bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
+<script src="{{ request()->root() }}/theme/bower_components/moment/min/moment.min.js"></script>
+<script src="{{ request()->root() }}/theme/bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
+<script src="{{ request()->root() }}/theme/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+<script src="{{ request()->root() }}/theme/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+<script src="{{ request()->root() }}/theme/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<script src="{{ request()->root() }}/theme/bower_components/fastclick/lib/fastclick.js"></script>
+<script src="{{ request()->root() }}/theme/dist/js/adminlte.min.js"></script>
+<script src="{{ request()->root() }}/theme/dist/js/pages/dashboard.js"></script>
+<script src="{{ request()->root() }}/theme/dist/js/demo.js"></script>
 </body>
 </html>
