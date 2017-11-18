097e52d74d9836481e6f5fd4020c7c2b5b633b15
HDMercado <31368475+HDMercado@users.noreply.github.com>
2017-11-18 18:49:39 -0300

    Update deleteVentas.blade.php
    estaba vacio

--- resources/views/deleteVentas.blade.php
+++ resources/views/deleteVentas.blade.php
@@ -1,5 +1,34 @@
 @extends('layout')
 
 @section('contenido')
-<h1>Contenido {{ request()->path() }}</h1>
+<table class="table table-striped"> 
+  <thead>
+    <tr>
+      <th>ID Venta</th>
+      <th>Fecha</th>
+      <th>Monto</th>
+    </tr>
+  </thead>
+  <tbody>
+
+      <tr>
+        <td><label for="IDVenta "></label></td>
+        <td><label for="Fecha"></label></td>
+        <td><label for="Monto"></label></td>
+      </tr>
+
+  </tbody>
+</table>
+
+<form class="form-group">
+  <div class="form-group">
+  </div>
+  <div class="form-group">
+    <label for="InputIDVenta">ID Venta</label>
+    <input type="text" class="form-control" id="InputIDVenta" placeholder="001">
+  </div>
+  <button type="submit" class="btn btn-default">Eliminar</button>
+    <div class="form-group">
+
+</form>
 @stop
