884a6917049497ad2549dccdf3bb386f68d4c14c
HDMercado <31368475+HDMercado@users.noreply.github.com>
2017-11-18 18:51:04 -0300

    Update deleteUsuario.blade.php
    faltaba contenido

--- resources/views/deleteUsuario.blade.php
+++ resources/views/deleteUsuario.blade.php
@@ -1,5 +1,34 @@
 @extends('layout')
 
 @section('contenido')
-<h1>Contenido {{ request()->path() }}</h1>
+<table class="table table-striped"> 
+  <thead>
+    <tr>
+      <th>ID Usuario</th>
+      <th>Rut Ususario</th>
+      <th>Nombre Usuario</th>
+    </tr>
+  </thead>
+  <tbody>
+
+      <tr>
+        <td><label for="IDUsusario "></label></td>
+        <td><label for="RutUsuario"></label></td>
+        <td><label for="NomUsusario"></label></td>
+      </tr>
+
+  </tbody>
+</table>
+
+<form class="form-group">
+  <div class="form-group">
+  </div>
+  <div class="form-group">
+    <label for="InputIDUduario">ID Usuario</label>
+    <input type="text" class="form-control" id="InputIDUsuario" placeholder="001">
+  </div>
+  <button type="submit" class="btn btn-default">Eliminar</button>
+    <div class="form-group">
+
+</form>
 @stop
